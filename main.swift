2b5ad082f45206eab6b518dcfecc2b65da40a4de
triggeredcode <omkar.tripathi101@gmail.com>
2025-08-10 19:45:00 +0530

    timer-based capture working, 10s interval

--- main.swift
+++ main.swift
@@ -2,7 +2,8 @@ import Cocoa
 import CoreGraphics
 
 let displayID = CGMainDisplayID()
-let jpegQuality: CGFloat = 0.85  // hardcoded for now
+let jpegQuality: CGFloat = 0.85
+var captureCount = 0
 
 func captureScreen() -> CGImage? {
     return CGDisplayCreateImage(displayID)
@@ -13,8 +14,20 @@ func toJPEG(_ image: CGImage) -> Data? {
     return bitmap.representation(using: .jpeg, properties: [.compressionFactor: jpegQuality])
 }
 
-if let img = captureScreen(), let data = toJPEG(img) {
-    print("JPEG size: \(data.count / 1024) KB")
-    try? data.write(to: URL(fileURLWithPath: "/tmp/screen.jpg"))
-    print("Saved to /tmp/screen.jpg")
+func doCapture() {
+    captureCount += 1
+    if let img = captureScreen(), let data = toJPEG(img) {
+        let path = "/tmp/sentinel_\(captureCount).jpg"
+        try? data.write(to: URL(fileURLWithPath: path))
+        print("[\(captureCount)] Captured \(data.count / 1024) KB")
+    }
 }
+
+// Capture every 10 seconds
+print("Starting capture loop (10s interval)...")
+let timer = Timer.scheduledTimer(withTimeInterval: 10.0, repeats: true) { _ in
+    doCapture()
+}
+
+doCapture()  // Initial capture
+RunLoop.main.run()
