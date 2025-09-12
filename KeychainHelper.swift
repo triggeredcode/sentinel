5c9ce3f49c9bddecf907850f0515c853ceee8c2b
triggeredcode <omkar.tripathi101@gmail.com>
2025-09-12 22:10:00 +0530

    add keychain storage for auth token

---
+++ KeychainHelper.swift
@@ -0,0 +1,37 @@
+import Foundation
+import Security
+
+// Simple keychain wrapper for storing auth token
+struct KeychainHelper {
+    static let service = "com.sentinel.auth"
+    static let account = "token"
+    
+    static func saveToken(_ token: String) -> Bool {
+        guard let data = token.data(using: .utf8) else { return false }
+        
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+            kSecValueData as String: data
+        ]
+        
+        SecItemDelete(query as CFDictionary)
+        return SecItemAdd(query as CFDictionary, nil) == errSecSuccess
+    }
+    
+    static func getToken() -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+            kSecReturnData as String: true
+        ]
+        
+        var result: AnyObject?
+        guard SecItemCopyMatching(query as CFDictionary, &result) == errSecSuccess,
+              let data = result as? Data else { return nil }
+        
+        return String(data: data, encoding: .utf8)
+    }
+}
